1248936b3dd1fd2ce76ee6832990068bcd38b6ca
Reapimus <29674188+Reapimus@users.noreply.github.com>
2023-09-09 18:05:17 +1000

    Remove cache for premium status

--- project/helpers/premium.py
+++ project/helpers/premium.py
@@ -1,13 +1,7 @@
 from project import BotAPI
-from project.helpers.Cache import Cache
-
-premium_cache = Cache()
 
 async def get_user_premium_status(user_id):
     with BotAPI() as bot_api:
-        cached = premium_cache.get(user_id)
-        if cached is not None:
-            return cached
         try:
             roles = await bot_api.get_member_roles('aE9Zg6Kj', user_id)
         except Exception:
@@ -25,6 +19,5 @@ async def get_user_premium_status(user_id):
             cached = 1
         else:
             cached = 0
-        
-        premium_cache.set(user_id, cached)
+
         return cached
